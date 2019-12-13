248a1b6e3de2f9d3adf19e267b2b574d6d735bbb
cgotfryd <chasegotfryd@gmail.com>
2019-12-12 20:26:02 -0500

    exercise 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,2 +1,9 @@
 #program to greet user by name
 puts "What is your name?"
+user = gets.chomp
+puts "Hello, #{user}"
+
+puts "How old are you?"
+age = gets.chomp
+birthYear = 2019 - age.to_i
+puts "Oh, cool! You were born in #{birthYear}"
